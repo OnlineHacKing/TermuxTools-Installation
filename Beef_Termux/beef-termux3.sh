f58a565c9c9433d4df928446434403744b99cc7c
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2022-12-28 12:14:42 +0530

    Update and rename beef-termux2.sh to beef-termux3.sh

---
+++ Beef_Termux/beef-termux3.sh
@@ -0,0 +1,150 @@
+#!/data/data/com.termux/files/usr/bin/bash
+clear
+echo -e "\e[32m
+  ██████╗ ███╗   ██╗██╗     ██╗███╗   ██╗███████╗      
+ ██╔═══██╗████╗  ██║██║     ██║████╗  ██║██╔════╝      
+ ██║   ██║██╔██╗ ██║██║     ██║██╔██╗ ██║█████╗        
+ ██║   ██║██║╚██╗██║██║     ██║██║╚██╗██║██╔══╝        
+ ╚██████╔╝██║ ╚████║███████╗██║██║ ╚████║███████╗      
+  ╚═════╝ ╚═╝  ╚═══╝╚══════╝╚═╝╚═╝  ╚═══╝╚══════╝      \e[0m     
+  \e[33m                                                     
+██╗  ██╗ █████╗  ██████╗██╗  ██╗██╗███╗   ██╗ ██████╗ 
+██║  ██║██╔══██╗██╔════╝██║ ██╔╝██║████╗  ██║██╔════╝ 
+███████║███████║██║     █████╔╝ ██║██╔██╗ ██║██║  ███╗
+██╔══██║██╔══██║██║     ██╔═██╗ ██║██║╚██╗██║██║   ██║
+██║  ██║██║  ██║╚██████╗██║  ██╗██║██║ ╚████║╚██████╔╝
+╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝ \e[0m \e[91mV2.1 \e[0m
+                                                      
+                                                                                          
+\e[95m  +-+-+-+-+-+-+-+-+-+-+ +-+-+ + \e[0m  
+  |B|e|E|F| |i|n| |T|e|r|m|u|x|
+\e[36m  +-+-+-+-+-+-+-+-+-+-+ +-+-+ + \e[0m  
+ \e[91m        +-+-+-+-+-+-+ +-+-+-+-+-+-+-+ \e[0m 
+         |O|n|l|i|n|e| |H|a|c|k|i|n|g|
+   \e[34m      +-+-+-+-+-+-+ +-+-+-+-+-+-+-+  \e[0m
+     \e[92m             +-+-+-+-+-+   \e[0m 
+                  |S|U|M|A|N|
+\e[93m	          +-+-+-+-+-+ \e[0m
+\e[91m Website\e[0m   -  \e[96m www.termux.xyz \e[0m
+\e[32m Telegram\e[0m  -  \e[95m https://telegram.dog/OnlineHacking \e[0m
+\e[33m YouTube\e[0m   -  \e[94m https://youtube.com/onlinehacking2 \e[0m
+"  
+
+center() {
+  termwidth=$(stty size | cut -d" " -f2)
+  padding="$(printf '%0.1s' ={1..500})"
+  printf '%*.*s %s %*.*s\n' 0 "$(((termwidth-2-${#1})/2))" "$padding" "$1" 0 "$(((termwidth-1-${#1})/2))" "$padding"
+}
+
+# Loading spinner
+center " Loading..."
+source <(echo "c3Bpbm5lcj0oICd8JyAnLycgJy0nICdcJyApOwoKY291bnQoKXsKICBzcGluICYKICBwaWQ9JCEKICBmb3IgaSBpbiBgc2VxIDEgMTBgCiAgZG8KICAgIHNsZWVwIDE7CiAgZG9uZQoKICBraWxsICRwaWQgIAp9CgpzcGluKCl7CiAgd2hpbGUgWyAxIF0KICBkbyAKICAgIGZvciBpIGluICR7c3Bpbm5lcltAXX07IAogICAgZG8gCiAgICAgIGVjaG8gLW5lICJcciRpIjsKICAgICAgc2xlZXAgMC4yOwogICAgZG9uZTsKICBkb25lCn0KCmNvdW50" | base64 -d)
+
+echo
+center "*** Dependencies installation..."
+
+## Remove not working repositories
+rm $PREFIX/etc/apt/sources.list.d/*
+
+## Install gnupg required to sign repository
+# pkg install -y gnupg
+
+## Sign gushmazuko repository
+# curl -fsSL https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/gushmazuko-gpg.pubkey | gpg --dearmor | tee $PREFIX/etc/apt/trusted.gpg.d/gushmazuko-repo.gpg
+
+## Add gushmazuko repository to install ruby 2.7.2 version
+# echo 'deb https://github.com/gushmazuko/metasploit_in_termux/raw/master gushmazuko main'  | tee $PREFIX/etc/apt/sources.list.d/gushmazuko.list
+
+## Set low priority for all gushmazuko repository (for security purposes)
+## Set highest priority for ruby package from gushmazuko repository
+# echo '## Set low priority for all gushmazuko repository (for security purposes)
+# Package: *
+# Pin: release gushmazuko
+# Pin-Priority: 100
+
+## Set highest priority for ruby package from gushmazuko repository
+# Package: ruby
+# Pin: release gushmazuko
+# Pin-Priority: 1001' | tee $PREFIX/etc/apt/preferences.d/preferences
+
+# Purge installed ruby
+apt purge ruby -y
+rm -fr $PREFIX/lib/ruby/gems
+
+pkg upgrade -y -o Dpkg::Options::="--force-confnew"
+
+# needs binutils
+echo
+center "*** needs binutils installation..."
+
+pkg install -y binutils python autoconf bison clang coreutils curl findutils apr apr-util postgresql openssl readline libffi libgmp libpcap libsqlite libgrpc libtool libxml2 libxslt ncurses make ncurses-utils ncurses git wget unzip zip tar termux-tools termux-elf-cleaner pkg-config git ruby -o Dpkg::Options::="--force-confnew"
+pkg install -y curl wget libyaml bison espeak nodejs 
+
+python3 -m pip install --upgrade pip
+python3 -m pip install requests
+
+
+# if any weird warning occurs maybe its becoze of bigdecimal & pg_ext.so . try comment those lines if problem persist
+
+echo
+center "*** Fix ruby BigDecimal"
+source <(curl -sL https://github.com/termux/termux-packages/files/2912002/fix-ruby-bigdecimal.sh.txt)
+
+echo
+center "*** Erasing old metasploit folder..."
+rm -rf $PREFIX/opt/metasploit-framework
+
+echo
+center "*** Downloading..."
+cd $PREFIX/opt
+git clone https://github.com/beefproject/beef --depth=1
+termux-open-url https://termux.xyz/how-to-install-beef-on-termux/
+
+echo
+center "*** Installation..."
+
+
+
+#sed -i "277,\$ s/2.8.0/2.2.0/" Gemfile.lock
+
+gem install bundler
+declare NOKOGIRI_VERSION=$(cat Gemfile.lock | grep -i nokogiri | sed 's/nokogiri [\(\)]/(/g' | cut -d ' ' -f 5 | grep -oP "(.).[[:digit:]][\w+]?[.].")
+#sed 's|nokogiri (1.*)|nokogiri (1.8.0)|g' -i Gemfile.lock
+
+gem install nokogiri -v $NOKOGIRI_VERSION -- --use-system-libraries
+
+# for aarch64 if nokogiri problem persist do this 
+
+bundle config build.nokogiri "--use-system-libraries --with-xml2-include=$PREFIX/include/libxml2"; bundle install
+
+gem install actionpack
+bundle update activesupport
+bundle update --bundler
+bundle install -j$(nproc --all)
+
+#$PREFIX/bin/find -type f -executable -exec termux-fix-shebang \{\} \;
+
+termux-elf-cleaner $PREFIX/lib/ruby/gems/*/gems/pg-*/lib/pg_ext.so
+
+echo
+center "*"
+echo -e "\033[32m Suppressing Warnings\033[0m"
+
+
+# Warning occurs during payload generation
+sed -i '86 {s/^/#/};96 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/concurrent-ruby-1.0.5/lib/concurrent/atomic/ruby_thread_local_var.rb
+sed -i '442, 476 {s/^/#/};436, 438 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/logging-2.3.1/lib/logging/diagnostic_context.rb
+
+## openssl issue has been fixed 
+
+#sed -i '13,15 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/encryption_algorithm/functionable.rb
+#sed -i '14 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp256.rb
+#sed -i '14 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp384.rb
+#sed -i '14 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/hrr_rb_ssh-0.4.2/lib/hrr_rb_ssh/transport/server_host_key_algorithm/ecdsa_sha2_nistp521.rb
+
+
+echo
+center "*"
+echo -e '\033[32m Setup complete Installation Now. \n\e[93m: https://termux.xyz/how-to-install-beef-on-termux/\033[0m'
+termux-open-url https://termux.xyz/how-to-install-beef-on-termux/
+center "*"
