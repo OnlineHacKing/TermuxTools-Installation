242dfced7e7310cf6f78f95d11a6b395658dce0b
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2023-02-05 08:59:08 +0530

    Update metasploit.sh

--- Metasploit_Termux/metasploit.sh
+++ Metasploit_Termux/metasploit.sh
@@ -47,8 +47,8 @@ center() {
 center " Loading..."
 source <(echo "c3Bpbm5lcj0oICd8JyAnLycgJy0nICdcJyApOwoKY291bnQoKXsKICBzcGluICYKICBwaWQ9JCEKICBmb3IgaSBpbiBgc2VxIDEgMTBgCiAgZG8KICAgIHNsZWVwIDE7CiAgZG9uZQoKICBraWxsICRwaWQgIAp9CgpzcGluKCl7CiAgd2hpbGUgWyAxIF0KICBkbyAKICAgIGZvciBpIGluICR7c3Bpbm5lcltAXX07IAogICAgZG8gCiAgICAgIGVjaG8gLW5lICJcciRpIjsKICAgICAgc2xlZXAgMC4yOwogICAgZG9uZTsKICBkb25lCn0KCmNvdW50" | base64 -d)
 
-echo
-center "*** Dependencies installation..."
+echo 
+center "*** \e[33m Dependencies installation...\e[97m"
 
 ## Remove not working repositories
 rm $PREFIX/etc/apt/sources.list.d/*
