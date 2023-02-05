6085ea35a2ed7479774b02419d2060d526aa35b3
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2023-02-05 12:05:56 +0530

    Update metasploit.sh

--- Metasploit_Termux/metasploit.sh
+++ Metasploit_Termux/metasploit.sh
@@ -67,27 +67,6 @@ sleep 3
 ## Remove not working repositories
 rm $PREFIX/etc/apt/sources.list.d/*
 
-## Install gnupg required to sign repository
-# pkg install -y gnupg
-
-## Sign gushmazuko repository
-# curl -fsSL https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/gushmazuko-gpg.pubkey | gpg --dearmor | tee $PREFIX/etc/apt/trusted.gpg.d/gushmazuko-repo.gpg
-
-## Add gushmazuko repository to install ruby 2.7.2 version
-# echo 'deb https://github.com/gushmazuko/metasploit_in_termux/raw/master gushmazuko main'  | tee $PREFIX/etc/apt/sources.list.d/gushmazuko.list
-
-## Set low priority for all gushmazuko repository (for security purposes)
-## Set highest priority for ruby package from gushmazuko repository
-# echo '## Set low priority for all gushmazuko repository (for security purposes)
-# Package: *
-# Pin: release gushmazuko
-# Pin-Priority: 100
-
-## Set highest priority for ruby package from gushmazuko repository
-# Package: ruby
-# Pin: release gushmazuko
-# Pin-Priority: 1001' | tee $PREFIX/etc/apt/preferences.d/preferences
-
 # Purge installed ruby
 apt purge ruby -y
 rm -fr $PREFIX/lib/ruby/gems
@@ -101,7 +80,6 @@ python3 -m pip install --upgrade pip
 python3 -m pip install requests
 echo ""
 echo ""
-# if any weird warning occurs maybe its becoze of bigdecimal & pg_ext.so . try comment those lines if problem persist
 echo ""
 echo -e "\e[95m  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+ \e[0m  "
 echo -e "\e[93m                   Fix Ruby BigDecimal"
