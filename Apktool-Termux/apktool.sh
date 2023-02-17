6de7a02da351f1222520266aaf3bed4bbe85ce5a
Online Hacking <45653647+OnlineHacKing@users.noreply.github.com>
2023-02-17 20:41:47 +0530

    Update by https://www.onlinehacking.org

--- Apktool-Termux/apktool.sh
+++ Apktool-Termux/apktool.sh
@@ -1,51 +1,48 @@
 
 banner ( ) { 
+toilet -F border -f mono12  "Apktool"  | lolcat
+printf " "
+printf "\e[1;34m[*] \e[1;96mCreated by \e[1;33m Termux XYZ ( https://termux.xyz )\e[0m"
+printf " "
+}
                   
-                  toilet -F border -f mono12  "Apktool"  | lolcat
-                  printf ""
-                  printf "\e[1;34m[*] \e[1;96mCreated by \e[1;33m Termux XYZ ( https://termux.xyz )\e[0m"
-                  }
-                  
-                  wr  ( )  {
-                               printf "\033[1;91m Invalid input!!!\n"
-                               selection
-                               }
-                               Apktool() {
-                                                 cd files
-                                                 export apktool_version=2.7.0
-
+wr  ( )  {
+printf "\033[1;91m Invalid input!!!\n"
+selection
+}
+                               
+Apktool() {
+cd files
+export apktool_version=2.7.0
 sh -c 'wget https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_$apktool_version.jar -O $PREFIX/bin/apktool.jar'
-
 chmod +r $PREFIX/bin/apktool.jar
-
 sh -c 'wget https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/linux/apktool -O $PREFIX/bin/apktool' && chmod +x $PREFIX/bin/apktool
+bash apktool.sh
+}
                                                  
-                                                 cd ~/Apktool-Termux ; bash apktool.sh
-                                                 
-                                                 }
-                                                 Java() {
-                                                               cd files
-                                                               bash java.sh
-                                                               cd ~/Apktool-Termux ; bash apktool.sh
-                                                               }
+Java() {
+bash files/java.sh
+bash apktool.sh
+}
                                                                
-                                 selection () {
-                                            cd ~/Apktool-Termux
-                                            echo -e -n "\e[1;96m Apktool\e[1;96m Termux : \e[0m"
-                                            read a
-                                            case $a in
-                                            1) Apktool ;;
-                                            2) Java ;;
-                                            3) exit ;;
-                                            *) wr ;;
-                                            esac
-                                            }
+selection () {
+echo -e -n "\e[1;96m ApkTool\e[1;96m Termux : \e[0m"
+read a
+case $a in
+ 1) Apktool ;;
+ 2) Java ;;
+ 3) exit ;;
+ *) wr ;;
+esac
+}
                                  
-                  menu () {
-                                  banner
-                                  printf "\n\033[1;91m[\033[0m1\033[1;91m]\033[1;92m Apktool \n"
-                                  printf "\033[1;91m[\033[0m2\033[1;91m]\033[1;92m Java ( If Apktool Don't Work )\n"
-                                  printf "\033[1;91m[\033[0m3\033[1;91m]\033[1;92m Exit\n\n\n"
-                                  selection
-                                  }
-                  menu
+menu () {
+banner
+printf " "
+printf "\n\033[1;91m[\033[0m1\033[1;91m]\033[1;92m Apktool \n"
+printf "\033[1;91m[\033[0m2\033[1;91m]\033[1;92m Java ( If Apktool Don't Work )\n"
+printf "\033[1;91m[\033[0m3\033[1;91m]\033[1;92m Exit\n\n\n"
+selection
+}
+
+menu
